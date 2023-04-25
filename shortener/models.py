ca9fc8f82f61cbd9f80ce01fb22c7fb20f3c8d32
JunO-93 <dlwnsdh17@naver.com>
2023-04-25 20:56:55 +0900

    Django URLs, Views, Models 1

--- shortener/models.py
+++ shortener/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # 기존에 만들어진 테이블을 커스텀 하기 위해 사용 ( 컬럼추가 )
 from django.contrib.auth.models import AbstractUser 
-from django.contrib.auth.models import User as U
+from django.contrib.auth.models import User
 from django.conf import settings
 
 # Create your models here.
@@ -12,14 +12,16 @@ class PayPlan(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
-#AbstractUser : 변경됬는지 2개 같이 못씀 users, UserDetail --> 강의에선 같이 써도 된다했지만 충돌나는듯
-# 해결방법 :https://stackoverflow.com/questions/67747391/field-defines-a-relation-with-the-model-auth-user-which-has-been-swapped-out 참고
-# UserDetaild의 OneToOneField에 to 옵션에다가 settings.AUTH_USER_MODEL 경로를 정의해서 알려주니 오류 발생안함
-class users(AbstractUser):
+#AbstractUser : 프로젝트 도중에 변경이 어려움 최초생성 시만 사용하는게 좋음ß / 같이 쓸때 OneToOneField User자리에 class Users가 들어가야 함
+# 이유 User을 상속받는게 아니라 Users를 상속하게하여 테이블을 바라보게 해야함 
+# 다른 해결방법 :https://stackoverflow.com/questions/67747391/field-defines-a-relation-with-the-model-auth-user-which-has-been-swapped-out 참고
+# UserDetaild의 OneToOneField에 to 옵션에다가 settings.AUTH_USER_MODEL 경로를 정의해서 알려주니 오류 발생안함 
+
+class Users(AbstractUser):
     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING) # AUTH_USER_MODEL = "shortener.Users" : settings.py 정의 필요
 
 
-# class UserDetail(models.Model):
-#     user = models.OneToOneField(to=settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
-#     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
+class UserDetail(models.Model):
+    user = models.OneToOneField( Users, on_delete=models.CASCADE) #to=settings.AUTH_USER_MODEL
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
  
--- shortener/views.py
+++ shortener/views.py
@@ -3,19 +3,19 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 
-# def index(request):
-#     # django ORM
-#     user = Users.objects.filter(username="admin").first() #django ORM
-#     email = user.email if user else "Anonymous User!"
-#     print(email)
-#     print(request.user.is_authenticated)
+def index(request):
+    # django ORM
+    user = Users.objects.filter(username="admin").first() #django ORM
+    email = user.email if user else "Anonymous User!"
+    print(email)
+    print(request.user.is_authenticated)
 
-#     if request.user.is_authenticated is False :
-#         email="Anonymous User!"
-#         print(email)
-#     return render(request, "base.html", {"welcome_msg": "Hello FastCampus!"})
+    if request.user.is_authenticated is False :
+        email="Anonymous User!"
+        print(email)
+    return render(request, "base.html", {"welcome_msg": f"Hello {email}!","hello":"world"})
 
 
-# def redirect_test(request):
-#     print("Go Redirect")
-#     return redirect("index")
+def redirect_test(request):
+    print("Go Redirect")
+    return redirect("index")
